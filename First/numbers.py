e85dc00b45fb1d590cdb085b7e12e2d7422f6339
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-02-26 15:30:41 +0300

    Commit 1 numbers.py
    Added 3 functions:
    minus, mult, division

--- First/numbers.py
+++ First/numbers.py
@@ -1,5 +1,17 @@
 def summ(x, y):
     return x + y
 
+def minus(x, y):
+    return x - y
 
-print(summ(5, 15))
+def multi(x, y):
+    return x * y
+
+def division(x, y):
+    return x / y
+
+
+print(summ(15, 5))
+print(minus(15, 5))
+print(multi(15, 5))
+print(int(division(15, 5)))
