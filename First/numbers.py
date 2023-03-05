8c242a8af19884f73a4c7cc393ae01ba600321fc
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-03-05 11:21:37 +0300

    Studying '__name__ == __ main__' Added function 'main'

--- First/numbers.py
+++ First/numbers.py
@@ -11,14 +11,20 @@ def multi(x, y):
 
 
 def division(x, y):
-    return x / y
+    return round(x / y, 2)
+
+
+def main():
+    a, b = (input('Enter 2 integer numbers, separated by space button, please: ')).split(' ')
+    print(summ(int(a), int(b)))
+    print(minus(int(a), int(b)))
+    print(multi(int(a), int(b)))
+    print(division(int(a), int(b)))
 
 
 if __name__ == '__main__':
     print(f'Запуск программы {__name__}')
-    print(summ(15, 5))
-    print(minus(15, 5))
-    print(multi(15, 5))
-    print(int(division(15, 5)))
+    main()
 else:
     print(f'Запуск импортированного модуля {__name__}.py')
+    print('Модуль содержит: \n- функцию summ (сложение), \n- функцию minus (вычитание), \n- функцию multi (умножение), \n- функцию division (деление с округлением до 2х знаков после запятой) ')
