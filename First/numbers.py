b679f0cbe4969a592bcfbcdc353125fb6d1b7070
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-03-05 11:29:08 +0300

    Studying file, contenting '__name__ == __ main__ - fixing syntax problems

--- First/numbers.py
+++ First/numbers.py
@@ -15,7 +15,9 @@ def division(x, y):
 
 
 def main():
-    a, b = (input('Enter 2 integer numbers, separated by space button, please: ')).split(' ')
+    a, b = (input('Enter 2 integer numbers, '
+                  'separated by space button, '
+                  'please: ')).split(' ')
     print(summ(int(a), int(b)))
     print(minus(int(a), int(b)))
     print(multi(int(a), int(b)))
@@ -27,4 +29,9 @@ if __name__ == '__main__':
     main()
 else:
     print(f'Запуск импортированного модуля {__name__}.py')
-    print('Модуль содержит: \n- функцию summ (сложение), \n- функцию minus (вычитание), \n- функцию multi (умножение), \n- функцию division (деление с округлением до 2х знаков после запятой) ')
+    print('Модуль содержит:'
+          ' \n- функцию summ (сложение),'
+          ' \n- функцию minus (вычитание),'
+          ' \n- функцию multi (умножение),'
+          ' \n- функцию division (деление '
+          'с округлением до 2х знаков после запятой) ')
